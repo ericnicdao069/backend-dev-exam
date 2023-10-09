9d702e79479c919c58daf1a36bf6b7af6c08bf28
Eric Nicdao <topsykrett19@gmail.com>
2023-10-09 14:15:17 +0800

    Add Inertia Product List and Create

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\ServiceProvider;
+use Inertia\Inertia;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +22,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         JsonResource::withoutWrapping();
+        Inertia::setRootView('layouts.app');
     }
 }
